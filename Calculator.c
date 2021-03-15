fc744f15d16a8b5a5109d34a2c8f5901a3cf4608
HEXecuter <80224568+HEXecuter@users.noreply.github.com>
2021-03-14 20:48:27 -0700

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -3,30 +3,36 @@
 #define SYNTAX_ERROR "SYNTAX ERROR ... RESTARTING\n\n"
 
 /*
-This function accepts two floats that represent operands, and an operator. Return NAN if there is an error 
+This function accepts two float pointers that represent operands, and an operator. Return NAN if there is an error 
 */
-float solve_equation(float first_oper, char op, float second_oper) {
+float solve_equation(float *first_oper, char op, float *second_oper) {
+	if (isinf(*first_oper) || isinf(*second_oper))
+	{
+		printf("ERROR: One of the floats overflowed");
+		return NAN;
+	}
+
 	switch (op) {
 	case '+':
-		return first_oper + second_oper;
+		return *first_oper + *second_oper;
 		break;
 
 	case '-':
-		return first_oper - second_oper;
+		return *first_oper - *second_oper;
 		break;
 
 	case '/':
 		// If dividing by zero, return NAN
-		if (second_oper == 0) {
+		if (*second_oper == 0) {
 			fprintf(stderr, "ERROR: Division by zero");
 			return NAN;
 			break;
 		}
 		else {
-			return first_oper / second_oper;
+			return *first_oper / *second_oper;
 		}
 	case '*':
-		return first_oper * second_oper;
+		return *first_oper * *second_oper;
 
 	// Return NAN for invalid numbers
 	default:
@@ -37,7 +43,7 @@ float solve_equation(float first_oper, char op, float second_oper) {
 }
 
 /*
-* Continously ask for an expression
+* Main function will prompt the user for their expression and evaluate it using the solve_equation function
 */
 int main(void) {
 	float f_operand, s_operand, solution;
@@ -59,23 +65,23 @@ int main(void) {
 		else
 			printf(SYNTAX_ERROR);
 
-		//Grab the operator inserted by user
+		//Grab the operator entered by user
 		if (scanf_s(" %c", &oper) == 1)
 			n_terms++;
 		else
 			printf(SYNTAX_ERROR);
 
-		//Grab the second number inserted by user
+		//Grab the second number entered by user
 		if (scanf_s(" %f", &s_operand) == 1)
 			n_terms++;
 		else
 			printf(SYNTAX_ERROR);
 
 		
-		//If I have all three terms, send calculation, else something went wrong
-		if (n_terms == 3) {
+		//If the 2 operands and 1 operator is valid, call solve_equation and print solution, else something went wrong
+		if (n_terms == 3 ) {
 			printf("%.2f %c %.2f = ", f_operand, oper, s_operand);
-			solution = solve_equation(f_operand, oper, s_operand);
+			solution = solve_equation(&f_operand, oper, &s_operand);
 			if (isnan(solution)){
 				printf("\n\n");
 			}
